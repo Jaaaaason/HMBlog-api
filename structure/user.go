d3cd2ad4c680358ac5d7436419402b22e1e2b089
Jason <holdmybeer@zoho.com>
2018-11-05 13:35:51 +0800

    remove useless tag of user struct

--- structure/user.go
+++ structure/user.go
@@ -2,6 +2,6 @@ package structure
 
 // User the blog user struct
 type User struct {
-	Username     string `json:"username" bson:"username" db:"username"`
-	PasswordHash []byte `bson:"password_hash" db:"password_hash"`
+	Username     string `json:"username" bson:"username"`
+	PasswordHash []byte `bson:"password_hash"`
 }
