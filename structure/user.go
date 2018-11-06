816faf2b68e42c530ac76581787ec67b02356338
Jason <holdmybeer@zoho.com>
2018-11-06 23:49:16 +0800

    add field ID to User struct

--- structure/user.go
+++ structure/user.go
@@ -1,7 +1,10 @@
 package structure
 
+import "github.com/globalsign/mgo/bson"
+
 // User the blog user struct
 type User struct {
-	Username     string `json:"username" bson:"username"`
-	PasswordHash []byte `bson:"password_hash"`
+	ID           bson.ObjectId `json:"id" bson:"_id,omitempty"`
+	Username     string        `json:"username" bson:"username"`
+	PasswordHash []byte        `json:"-" bson:"password_hash"`
 }
